3c85c98f428e1d6e25495c3c4fe1e0e75275fd79
123slp <2192530185@qq.com>
2025-03-25 23:09:09 +0800

    Revert "第3次提交"
    This reverts commit d878a7e00220c8b8c328b36606ff9e1e71de5593.

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 console.log(1111)
-console.log(2222)
 const app = createApp(App)
 
 app.use(createPinia())
